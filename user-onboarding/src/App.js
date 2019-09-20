b75af1ee18d254f5521c65a34cac815dd3150f3b
Dale Pancho <dalepancho7@gmail.com>
2019-09-19 18:52:44 -0700

    MVP

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserForm from "./Form";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UserForm/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
